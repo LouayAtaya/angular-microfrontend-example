f18e62ec1ef88de164b95fb4e17f6467731a04b2
Mhd Louay Ataya <86228931+LouayAtaya@users.noreply.github.com>
2023-09-27 16:53:51 +0300

    dsfds

--- projects/host/src/app/app-routing.module.ts
+++ projects/host/src/app/app-routing.module.ts
@@ -4,32 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
-  // {
-  //   path: 'catalog',
-  //   loadChildren: () => loadRemoteModule({
-  //     remoteEntry: 'http://localhost:4201/remoteEntry.js',
-  //     // remoteName: 'remoteCatalog',
-  //     exposedModule: './ItemsModule',
-  //     type:'module'
-  //   }).then(m => m.AddressModule)
-  // },
-  {
-    path: 'catalog',
-    loadChildren: () => import('remoteCatalog/ItemsModule').then(m => m.ItemsModule)
-  },
-  {
-    path: 'orders',
-    loadChildren: () => import('remoteOrdering/OrdersModule').then(m => m.OrdersModule)
-  },
-  {
-    path: 'export-orders',
-    loadChildren: () => import('remoteOrdering/ExportOrdersModule').then(m => m.ExportOrdersModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('remoteUserManagement/LoginModule').then(m => m.LoginModule)
-  },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
 ];
 
 @NgModule({ 
